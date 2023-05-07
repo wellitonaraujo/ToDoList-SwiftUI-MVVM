af50943ffcdab53f8966db97bd33debbf704dcb2
wellitonaraujo <wellitonaraujodev@hotmail.com>
2023-05-07 12:15:03 -0300

    create account screen

--- TodoList/Views/ToDoListView.swift
+++ TodoList/Views/ToDoListView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewViewModel()
+    
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
     var body: some View {
-        Text("Welcome")
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    //Action
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView()
+        ToDoListView(userId: "")
     }
 }
